6c3fd7f9cd16a5af23847dc20e193eed74a21159
MatweyNazarenko <nazarehko1971@gmail.com>
2026-09-24 18:54:31 +0300

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from "./App";
-import "./styles.css";
+import App from './App';
+import './styles.css';
+
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW({ immediate: true });
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,55 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+const manifest = {
+	theme_color: '#ff9e42',
+	background_color: '#ffffff',
+	icons: [
+		{
+			purpose: 'maskable',
+			sizes: '512x512',
+			src: 'icon512_maskable.png',
+			type: 'image/png',
+		},
+		{
+			purpose: 'any',
+			sizes: '512x512',
+			src: 'icon512_rounded.png',
+			type: 'image/png',
+		},
+	],
+	screenshots: [
+		{
+			src: '/screenshots/desctop.png',
+			type: 'image/png',
+			sizes: '1900x940',
+			form_factor: 'wide',
+		},
+		{
+			src: '/screenshots/mobile.png',
+			type: 'image/png',
+			sizes: '264x840',
+			form_factor: 'narrow',
+		},
+	],
+	orientation: 'any',
+	display: 'standalone',
+	dir: 'auto',
+	lang: 'ru',
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/MAK/',
-})
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			workbox: {
+				globPatterns: ['**/*{html, css, js, ico, webp, png, svg}'],
+			},
+			manifest: manifest,
+		}),
+	],
+	base: '/MAK/',
+});
